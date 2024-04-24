87360f1970b2284f54a990c84555ccbaff2e8bad
Diego Carmona <ultgojifan@gmail.com>
2024-04-23 23:39:13 -0700

    Added model of Cats with 5 properties. Age, weight, name, species, and Regioin of origin.

---
+++ COMP003B.Assignment/Models/CatModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment.Models
+{
+    public class CatModel
+    {
+        public int Weight { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string RegionOfOrigin { get; set; }
+        public int Age { get; set; }
+    }
+}
